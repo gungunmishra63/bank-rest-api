d3e9d883eb6ae22704cb86023b91aaeb6fac1c13
Gungun Mishra <gungunm940@gmail.com>
2026-08-06 14:03:41 +0530

    Updated withdraw API and main routes

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from fastapi import FastAPI, status
 from model import AccountCreate, Transaction
 from service import bank_service
+
+# Create FastAPI application
 app = FastAPI(
     title="Banking REST API"
 )
-@app.get("/")# decorator 
+
+# Home API
+@app.get("/")
 def home():
     return {
         "message": "Welcome to Banking REST API"
     }
+
 # Create Account
 @app.post(
     "/accounts",
@@ -16,30 +21,34 @@ def home():
 )
 def create_account(account: AccountCreate):
     return bank_service.create_account(account)
-# Deposit the  money
+
+# Deposit Money
 @app.post("/accounts/{account_number}/deposit")
 def deposit_money(
-        account_number: str,
-        transaction: Transaction
-): 
+    account_number: str,
+    transaction: Transaction
+):
     return bank_service.deposit(
         account_number,
         transaction.amount
     )
-# Withdraw the money
+
+# Withdraw Money
 @app.post("/accounts/{account_number}/withdraw")
 def withdraw_money(
-        account_number: str,
-        transaction: Transaction
+    account_number: str,
+    transaction: Transaction
 ):
     return bank_service.withdraw(
         account_number,
         transaction.amount
     )
 
+# Get Account Details
 @app.get("/accounts/{account_number}")
 def get_account(account_number: str):
     return bank_service.get_account(account_number)
+
 # Get All Accounts
 @app.get("/accounts")
 def get_all_accounts():
